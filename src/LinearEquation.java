1edc4626fd9e28498a4229074e3dce22473558b5
ksoa0123 <koushiks@nycstudents.net>
2022-10-19 12:28:02 -0400

    added new file

---
+++ src/LinearEquation.java
@@ -0,0 +1,3 @@
+public class LinearEquation {
+    // Instance Variables
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+//Runner Class
 public class Main {
     public static void main(String[] args){
-        // Code 
+        Scanner scan = new Scanner(System.in);
+        //  Ex; (0,1) & (2,4)
+
+        String testCoord = "(0,3)";
+
+
+        //Remove the paranthesis so you can use parseInt() . if (.contains) a parantehsis, To find the next number use substring so you can do number and then substring indexOf
+
+
+        if (testCoord)
+        System.out.println(stringAsInt);
+
+        //You this file to ask for inputs and then use the methods that you make in the other file to computate the values and then it will all be printed here.
+
     }
 }
