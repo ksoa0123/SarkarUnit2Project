462390bbe9ce1833a25561cc0db36e425aeba217
kosausrk <kosausrk@gmail.com>
2022-10-21 16:37:39 -0400

    Added class to Main file

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -2,11 +2,13 @@ public class LinearEquation {
     int xCoord;
     int yCoord;
 
-    public void Coordinate(String coord){
+    public void LinearEquation(String coord){
         coord = coord.substring(1, coord.length() -1);  //Gets rid of paranthesis
 
         this.xCoord = Integer.parseInt(coord.substring(0, coord.indexOf(","))); //gets x coordinate from string
 
         this.yCoord = Integer.parseInt(coord.substring(coord.indexOf(",") + 2, coord.length())); //gets y value **remember the space after the comma
     }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,28 @@
 import java.util.Scanner;
 //Runner Class
 
-//Testing Intelijj Github Integration.
-
 public class Main {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         //  Ex; (0,1) & (2,4)
 
-        String testCoord = "(0, 3)";
+        System.out.println("Enter coordinate: ");
+
+        String strCoord = scan.nextLine();
+
+
+        LinearEquation coordinate = new LinearEquation("(0,3)");
+
+        System.out.println(coordinate.xCoord);
+        System.out.println(coordinate.yCoord);
+
+
 
 
         //Remove the paranthesis so you can use parseInt() . if (.contains) a parantehsis, To find the next number use substring so you can do number and then substring indexOf
-        String cleanCoord = testCoord.substring((1, testCoord.length() -1 )); //Removes the Paranthwesis
+         //Removes the Paranthwesis
 
-        int xCoord = cleanCoord.substring(0, cleanCoord.indexOf(","))
+
 
         //You this file to ask for inputs and then use the methods that you make in the other file to computate the values and then it will all be printed here.
 
