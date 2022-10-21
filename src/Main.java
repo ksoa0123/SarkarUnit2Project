112981fd23f74207a894e0e789ec0cf368c4ecad
ksoa0123 <koushiks@nycstudents.net>
2022-10-21 12:34:38 -0400

    found x and y coordinate in LinearEquation

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,13 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         //  Ex; (0,1) & (2,4)
 
-        String testCoord = "(0,3)";
+        String testCoord = "(0, 3)";
 
 
         //Remove the paranthesis so you can use parseInt() . if (.contains) a parantehsis, To find the next number use substring so you can do number and then substring indexOf
+        String cleanCoord = testCoord.substring((1, testCoord.length() -1 )); //Removes the Paranthwesis
 
-
-        if (testCoord)
-        System.out.println(stringAsInt);
+        int xCoord = cleanCoord.substring(0, cleanCoord.indexOf(","))
 
         //You this file to ask for inputs and then use the methods that you make in the other file to computate the values and then it will all be printed here.
 
