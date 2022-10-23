d78fbc90be757df44534ec06a499f5951343d7b4
kosausrk <kosausrk@gmail.com>
2022-10-22 20:55:04 -0400

    Fixed code to format coordinates from Main, and then perform operations in linear equation file

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,17 +1,9 @@
 public class LinearEquation {
-    int xCoord;
-    int yCoord;
 
-    String debug;
+    //Make sure to scan if same 2 x values correspond to different y's **Fails vertical line text
 
-    //(0, 3) --> 0,
-    public LinearEquation(String coord){
-        String newCoord = coord.substring(1, coord.length()-1);  //Gets rid of paranthesis so we can use parseInt
 
-        this.xCoord = Integer.parseInt(newCoord.substring(0, newCoord.indexOf(","))); //gets x coordinate from string
-
-        this.yCoord = Integer.parseInt(newCoord.substring(newCoord.indexOf(",") + 2)); //gets y value **remember the space after the comma
-    }
+    //Methods: equation(), slope() , yInt(), distance(), lineInfo() , coordinateForX(double)
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,33 @@ public class Main {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         //  Ex; (0, 1) & (2, 4)
-
-        System.out.println("Enter coordinate: ");
-
-        String strCoord = scan.nextLine();
-
-        LinearEquation coordinate = new LinearEquation(strCoord);
-
-        System.out.println(coordinate.xCoord);
+        int x1, y1, x2, y2; //Sets up coordinate point values
 
 
-        System.out.println(coordinate.yCoord);
 
+
+        System.out.println("Hi user! Welcome to Linear Equation finder!"); //Greeting, as per rubric in doc
+
+        System.out.println("Enter coordinate in format (x, y) **including space: ");
+        String Coord1 = scan.nextLine(); //Gets user input of first coordinate in format (x, y)
+        Coord1 = Coord1.substring(1, Coord1.length()-1);  //Gets rid of paranthesis so we can use parseInt. Format returned: x, y
+        x1 = Integer.parseInt(Coord1.substring(0, Coord1.indexOf(","))); //gets x coordinate from string. Picks from index 0 to where comma apears **Recall 2nd parameter in substring is not inclusive so we get only x value
+        y1 = Integer.parseInt(Coord1.substring(Coord1.indexOf(",") + 2)); //gets y value **remember the space after the comma that's why we did +2
+
+
+
+
+        //Repeating same logic for 2nd coordinate
+        System.out.println("Enter 2nd coordinate in format (x, y) **including space: ");
+        String Coord2 = scan.nextLine();
+        Coord2 = Coord2.substring(1, Coord2.length()-1);
+
+        x2 = Integer.parseInt(Coord2.substring(0, Coord2.indexOf(",")));
+        y2 = Integer.parseInt(Coord2.substring(Coord2.indexOf(",") + 2));
+
+
+
+        System.out.println(String.format("Coordinate 1 (%s, %s) and Coordinate 2 (%s, %s)", x1, y1, x2, y2));
 
     }
 }
